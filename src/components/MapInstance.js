ab474a1bfde6471485758926a971e45704fa3128
hider brandon vargas <hiderbrandon@gmail.com>
2023-03-14 23:38:06 -0500

    draw control added not working

--- src/components/MapInstance.js
+++ src/components/MapInstance.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
-import Map, { useControl, Source, Layer, FullscreenControl } from 'react-map-gl';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import Map, { useControl, Source, Layer, FullscreenControl, GeolocateControl } from 'react-map-gl';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import { myConfig } from "../config.js";
 import lineOffset from '@turf/line-offset';
 import { lineString } from '@turf/helpers';
-import { getCoords } from '@turf/invariant';
+import DrawControl from './DrawControl.js';
+
 
 const TOKEN = myConfig.mapboxToken;
 
@@ -23,14 +24,14 @@ function MapInstance() {
   });
 
   useEffect(() => {
-    if (selectedPoints.length >2) {
+    if (selectedPoints.length > 2) {
       const offset = lineOffset(line, 0.01, { units: 'kilometers' });
       setOffsetLine(offset);
     }
   }, [lineData, selectedPoints]);
 
   useEffect(() => {
-    if (selectedPoints.length >=2) {
+    if (selectedPoints.length >= 2) {
       const line = lineString(selectedPoints.map(p => [p.lng, p.lat]), { "stroke": "#F00" });
       setLine(line);
     }
@@ -48,6 +49,27 @@ function MapInstance() {
       setLineData(newLineData);
     }
   }, [selectedPoints]);
+
+
+  const onUpdate = useCallback(e => {
+    setLineData(currFeatures => {
+      const newFeatures = { ...currFeatures };
+      for (const f of e.features) {
+        newFeatures[f.id] = f;
+      }
+      return newFeatures;
+    });
+  }, []);
+
+  const onDelete = useCallback(e => {
+    setLineData(currFeatures => {
+      const newFeatures = { ...currFeatures };
+      for (const f of e.features) {
+        delete newFeatures[f.id];
+      }
+      return newFeatures;
+    });
+  }, []);
 
   const handleClick = (event) => {
     const { lngLat } = event;
@@ -69,9 +91,20 @@ function MapInstance() {
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-
         <FullscreenControl></FullscreenControl>
+        <GeolocateControl></GeolocateControl>
 
+        <DrawControl
+          position="top-left"
+          displayControlsDefault={true}
+          controls={{
+            line_string: true,
+          }}
+          defaultMode="draw_line_string"
+          onCreate={onUpdate}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+        />
 
         <Source id="my-data" type="geojson" data={lineData}>
           <Layer
@@ -90,11 +123,11 @@ function MapInstance() {
             type="line"
             paint={{
               'line-color': '#d703fc',
-              'line-width': 2 ,
+              'line-width': 2,
             }}
           />
         </Source>
-        
+
 
       </Map>
 
